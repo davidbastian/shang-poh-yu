e7074d21056d7411c565b451921546de145f7138
David Bastian <d@davidbastian.red>
2017-11-20 11:28:19 +1300

    chabged webpack server

--- tasks/optimize.images.js
+++ tasks/optimize.images.js
@@ -3,8 +3,12 @@ const imagemin = require('imagemin');
 
 const imageminJpegRecompress = require('imagemin-jpeg-recompress');
 const imageminMozjpeg = require('imagemin-mozjpeg');
+
+var input = path.join(__dirname, '../app/common/media/images/')+'*.jpg';
+var ouput = path.join(__dirname, '../dist/common/media/images/');
  
-imagemin([path.join(__dirname, '../app/common/media/images/')+'*.jpg'], path.join(__dirname, '../dist/common/media/images/'), {
+
+imagemin([input],ouput,{
     plugins: [
         imageminJpegRecompress({
             quality:'low',
